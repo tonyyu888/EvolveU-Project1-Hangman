4b528d90dac561d2d1c27fd055154ba760251df9
tonyyu888 <81333371+tonyyu888@users.noreply.github.com>
2021-04-29 13:09:15 -0600

    Implemented routing

--- routes/game.js
+++ routes/game.js
@@ -1,17 +1,59 @@
-require('./model/skyrim') // initialize all of the scenes
-
-const sceneRoutes = require('./routes/scene')
-
 const express = require('express')
-const app = express()
-const port = 3000 || process.env.PORT
+const gameSetting = require("../model/gameSettings");
+const gameScores = require("../model/gameScores");
 
-app.use('/scene', sceneRoutes)
+let router = express.Router();
 
-app.get('/', (req, res) => {
-    res.redirect('/scene/intro')
+router.get('/submitUserSelection', async (req, res) => {
+  try {
+      const findLetter = req.query.userLetter.toUpperCase();
+
+      gameProgressResult = await gameSetting.determineGameProgress(findLetter);
+
+      res.send({gameProgressResult});
+  }
+  catch (error) {
+      console.log(error);
+      res.status(404).send("There is a problem in submitUserSelection endpoint.\n");
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Game server listening at http://localhost:${port}`)
+router.get('/restartGame', async (req, res) => {
+  try {
+      gameProgressResult = await gameSetting.initializeGameVariables();
+      res.send({gameProgressResult}); 
+  }
+  catch (error) {
+      console.log(error);
+      res.status(404).send("There is a problem in restartGame endpoint.\n");
+  }    
 })
+
+router.get('/submitScore', async (req, res) => {
+  try {
+      const playerInitial = req.query.userInitial.toUpperCase();
+      gameDataResult = await gameSetting.saveUserScore();
+
+      await gameScores.addUserScore(playerInitial, gameDataResult.startDateTime, gameDataResult.endDateTime, gameDataResult.word, gameDataResult.letterNotFoundCount, gameDataResult.timeCompleted);
+
+      await res.send({status: "success"});
+  }
+  catch (error) {
+      console.log(error);
+      res.status(404).send("There is a problem in submitScore endpoint.\n");
+  }
+})
+
+router.get('/getScores', async (req, res) => {
+  try {
+      let scores = await gameScores.getScores();
+
+      res.send(await scores.toArray());
+  }
+  catch (error) {
+      console.log(error);
+      res.status(404).send("There is a problem in getScores endpoint.\n");
+  }
+})
+
+module.exports = router
